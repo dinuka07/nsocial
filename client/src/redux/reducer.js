e64d5cb9f2bd3f8f4b7ead4f5eedfa30722b0dc1
dinuka07 <dinukananayakkara1@gmail.com>
2024-02-10 17:16:50 +0530

    redux

--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -1,1 +1,13 @@
 import { combineReducers } from "@reduxjs/toolkit";
+
+import userslice from "./postSlice";
+import themeSlice from "./theme";
+import postSlice from "./postSlice";
+
+const rootReducer = combineReducers({
+    user: userslice,
+    theme: themeSlice,
+    posts: postSlice,
+});
+
+export { rootReducer};
