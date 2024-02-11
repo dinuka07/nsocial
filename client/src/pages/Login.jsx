5e64305843e5ab0f1ec3d326cedc576d9da09c53
dinuka07 <dinukananayakkara1@gmail.com>
2024-02-11 22:22:17 +0530

    login-complete

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import {useForm} from "react-hook-form";
 import { TbSocial} from "react-icons/tb";
+import {Bsshare} from "react-icons/bs";
 import {TextInput} from "../components";
 import { useDispatch } from "react-redux";
 import { CustomButton, Loading } from "../components";
@@ -10,8 +11,11 @@ const Login = () => {
   const {
     register, handleSubmit, formState: {errors},
   } = useForm({
-    mode: "onChange",
+    mode: "onChange" ,
   });
+  const onSubmit = async(data)=> {
+
+  }
 
   const [errMsg, setErrMsg] = useState("");
   const [isSubmitting, setIsSubmitting] = useState(false);
@@ -20,7 +24,7 @@ const Login = () => {
     <div className='bg-bgColor w-full h-[100vh] flex items-center justify-center p-6'>
       <div className='w-full md:w-2/3 h-fit lg:h-full 2xl:h-5/6 py-8 lg:py-0 flex bg-primary rounded-xl overflow-hidden shadow-xl'>
        {/*left */} 
-       <div className='w-full lg:w=1/2 h-full p-10 2xl:px-20 flex flex-col justify-center'>
+       <div className='w-full lg:w-1/2 h-full p-10 2xl:px-20 flex flex-col justify-center'>
         <div className='w-full flex gap-2 items-center mb-6'>
          <div className='p-2 bg-[#065ad8] rounded text-white'>
           <TbSocial/>
@@ -39,7 +43,8 @@ const Login = () => {
        </p>
        <span className='text-sm mt-2 text-ascent-2'>Welcome back</span>
 
-       <form className='py-8 flex flex-col gap-5'>
+       <form className='py-8 flex flex-col gap-5'
+       onSubmit={handleSubmit(onSubmit)}>
         <TextInput 
         name="email" placeholder="email@example.com"
 
@@ -111,7 +116,16 @@ const Login = () => {
        
        {/*right */}
        <div className='hidden w-1/2 h-full lg:flex flex-col items-center justify-center bg-blue'>
-        <div className=""></div>
+        <div className='relative w-full flex items-center justify-center'>
+          <img src="{BgImage}" alt='Bg Image'
+          className='w-48 2xl:w-64 h-48 2xl:h-64 rounded-full object-cover' />
+
+          <div className='absolute flex items-center gap-1 bg-white right-10
+          top-10 py-2 px-5 rounded-full'>
+            
+            <span className='text-xs font-medium'>Share</span>
+          </div>
+        </div>
        </div>
        <div></div>
 
