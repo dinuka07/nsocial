dae55e1f761fd69f9c3cd6b6cf4183ab319597a6
dinuka07 <dinukananayakkara1@gmail.com>
2024-02-28 09:04:36 +0530

    data

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,6 @@
 import TextInput from "./TextInput";
 import Loading from "./Loading";
 import CustomButton from "./CustomButton";
+import TopBar from "./TopBar";
 
-export { Loading, TextInput, CustomButton};
+export { Loading, TextInput, CustomButton ,TopBar};
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { UseSelector, useSelector } from 'react-redux';
+import { TopBar } from '../assets';
 
 const Home = () => {
+  const {user} = useSelector((state)=> state.user);
   return (
-    <div>Home</div>
-  )
-}
+    <div className='home w-full px-0 lg:px-10 pb-20 2xl:px-40 bg-bgColor lg:rounded-lg h-screen overflow-hidden'>
+      
+    </div>
+  );
+};
 
 export default Home
